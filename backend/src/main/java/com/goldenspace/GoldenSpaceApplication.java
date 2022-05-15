6cf5bc95d3e055a0afcb821e99b623f2684e143a
sly503 <fatjonrami@gmail.com>
2022-05-15 22:26:10 +0200

    first commit

---
+++ backend/src/main/java/com/goldenspace/GoldenSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.goldenspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenSpaceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/goldenspace/GoldenSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldenspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
